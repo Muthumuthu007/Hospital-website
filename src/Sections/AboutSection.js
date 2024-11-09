fb7ac7343423602356a186f59411e960956b1366
muthu k <muthuk@muthus-MacBook-Air.local>
2024-11-10 00:02:14 +0530

    Before Animation

--- src/Sections/AboutSection.js
+++ src/Sections/AboutSection.js
@@ -27,5 +27,4 @@ function AboutSection() {
     </section>
   );
 }
-
 export default AboutSection;
--- src/Sections/Footer.js
+++ src/Sections/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>Proprietor: Selvarani R&nbsp;&nbsp;&nbsp;&nbsp;GST Registration Number: 33MBEPS7815Q1ZO</p>
-      <p>Location: 10/11, Meenakchi Street, Laxmi Amman Nagar, Chennai, Tamil Nadu, 600118</p>
+      <p>Proprietor: Selvarani R&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GST Registration Number: 33MBEPS7815Q1ZO</p>
+      <p><b>Location:</b> 10/11, Meenakchi Street, Laxmi Amman Nagar, Chennai, Tamil Nadu, 600118</p>
     </footer>
   );
 }
